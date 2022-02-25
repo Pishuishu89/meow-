46d19b7aefb97931ce2f0d90db4a3b4334a07362
Pishuishu89 <66273019+Pishuishu89@users.noreply.github.com>
2022-02-25 12:35:22 -0500

    Update SleepCalculator.java
    added print statement

--- SleepCalculator.java
+++ SleepCalculator.java
@@ -20,6 +20,7 @@ public class SleepCalculator extends JFrame {
 
 
 	public static void main(String[] args) {
+		System.out.println("hello wrld");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
